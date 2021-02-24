63ad663f651742bde8dd5803a08cddfa9cad9120
brooderhall <dev.brooderhall@gmail.com>
2021-02-24 12:47:19 +0000

    default added

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -3,7 +3,7 @@ export const homeObjOne = {
     description: 'Acquire or hone the skill that will land you that dream job, or get you started on that project. Take a class now',
     imgStart: true,
     lightBg: false,
-    img: require('../../images/img-1.png'),
+    img: require('../../images/img-1.png').default,
     alt: 'Image',
 }
 
@@ -13,7 +13,7 @@ export const homeObjTwo = {
     description: 'Acquire or hone the skill that will land you that dream job, or get you started on that project. Take a class now',
     lightBg: true,
     imgStart: false,
-    img: require('../../images/img-2.png'),
+    img: require('../../images/img-2.png').default,
     alt: 'Image2',
 }
 
@@ -23,6 +23,6 @@ export const homeObjThree = {
     description: 'Acquire or hone the skill that will land you that dream job, or get you started on that project. Take a class now',
     lightBg: false,
     imgStart: true,
-    img: require('../../images/img-3.png'),
+    img: require('../../images/img-3.png').default,
     alt: 'Image3',
 }
