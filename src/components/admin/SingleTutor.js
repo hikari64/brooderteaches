e3b917561abb594230e55942dfcd61c3ee33455a
Antwi Isaac <antwisaac7@gmail.com>
2021-06-06 17:57:03 +0100

    Verifying tutor complete

--- src/components/admin/SingleTutor.js
+++ src/components/admin/SingleTutor.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-// booststrap imports
+// verify tutor
+import { verifyTutor } from "../../firebase";
 
-import { Button, Modal } from "react-bootstrap";
+// booststrap imports
+import { BsFillPersonCheckFill } from "react-icons/bs";
+
+import { Button, Modal, Container, Table, Row, Col } from "react-bootstrap";
 
 export default function SingleTutor(props) {
   const tutor = props.tutor;
@@ -35,10 +39,50 @@ export default function SingleTutor(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            {tutor.name}'s Profile
+            <Container fluid className="px-4">
+              {tutor.firstName}'s Profile
+            </Container>
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body className="modal-main"></Modal.Body>
+        <Modal.Body className="modal-main">
+          <Container fluid className="px-4">
+            {/* Name and verification */}
+            <Row>
+              <Col>
+                <h1>
+                  {tutor.lastName}, {tutor.firstName}
+                </h1>
+                <div>{tutor.email}</div>
+              </Col>
+              <Col>
+                {tutor.verified ? (
+                  <React.Fragment>
+                    <div>
+                      <BsFillPersonCheckFill size={40} color="#75DDDD" />{" "}
+                      <span className="ms-2">
+                        <strong>Verified</strong>
+                      </span>
+                    </div>
+                  </React.Fragment>
+                ) : (
+                  <Button variant="success" onClick={() => verifyTutor(tutor)}>
+                    Verify Tutor
+                  </Button>
+                )}
+              </Col>
+            </Row>
+            <hr className="mt-4 mb-4" />
+            <Row>
+              <h4>Identification Card</h4>
+            </Row>
+            <hr className="mt-4 mb-4" />
+            {/* personal details */}
+            <Row>
+              <h4>Other details</h4>
+              <p>Contact: {tutor.contact}</p>
+            </Row>
+          </Container>
+        </Modal.Body>
         <Modal.Footer>
           <Button onClick={() => setModalShow(false)}>Close</Button>
         </Modal.Footer>
